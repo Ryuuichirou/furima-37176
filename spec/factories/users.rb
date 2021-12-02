b1876e0ab645f9d5a297dfe73269e7087c36bc3f
ryuuichirou <kmryu0319@icloud.com>
2021-12-02 20:38:07 +0900

    テストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nick_name              {'test'}
+    first_name             {'山田'}
+    last_name              {'陸太郎'}
+    first_name_kana        {'ヤマダ'}
+    last_name_kana         {'リクタロウ'}
+    birthday               {'1999-02-01'}
+    email                  {Faker::Internet.free_email}
+    password               {'abc111'}
+    password_confirmation  {password}
+  end
+end
